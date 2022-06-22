ed8b3859b78b667353609aa49b1ec37835564abc
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-06-22 13:29:11 -0500

    update To WeatherApp

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-const MAPBOX_API_KEY = 'pk.eyJ1IjoieW9zb3lhcm9kIiwiYSI6ImNsM2x5OWFzaTAxdmIzanBvN21saTBhZjcifQ.rnFtIetzgEsDKSfY5G-zRg';
-
-const OPEN_WEATHER_API_KEY = '2069c5aeaeedf7d51e7b8dad1583ad71'
-
-const myGithubToken = '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';
